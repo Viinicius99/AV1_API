22f8e392ac01f3e9679675ce950d993a0ec4aff8
EA - Vinicius Oliveira Da Cunha Claro <vinicius_claro@educadventista.org>
2026-02-26 10:00:27 -0300

    rota GET

---
+++ src/dados.js
@@ -0,0 +1,9 @@
+export const tarefas = [
+  { id: 1, titulo: "Estudar Node", concluida: false },
+  { id: 2, titulo: "Fazer telas no Figma", concluida: true }
+]
+
+// if (!titulo || titulo.trim() === "") {
+// return res.status(400).json({ erro: "Título é obrigatório." })
+// ;
+// }
--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 import express from "express"
+import { tarefas} from "./dados.js"
 
-const app = express(); 
-const PORTA = 3000;
+const app = express();
+
+const PORT = 3000;
 
 app.use(express.json());
 
+// GET /tarefas lista o array inteiro
+app.get("/tarefas", (req, res) => {
+res.status(200).json(tarefas);
+});
+// POST /tarefas cria uma nova tarefa
+app.post("/tarefas", (req, res) => {
+const { titulo } = req.body;
+});
 
-// GET /tarefas lista o array inteiro 
-app.get("/tarefas", (req, res) => { 
-res.status(200).json(tarefas); 
-}); 
-// POST /tarefas cria uma nova tarefa 
-app.post("/tarefas", (req, res) => { 
-const { titulo } = req.body; 
-
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
